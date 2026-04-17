05d008acbd97bcd905bf3f7f15b82fbeda6dc97b
Kiara Bartuccio <kiarabartuccio@icloud.com>
2026-04-16 23:12:38 -0400

    Finished firebase auth changes

---
+++ blueprint/lib/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_project/auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final User? user = Auth().currentUser;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
+  Widget title() {
+    return const Text('Firebase Auth');
+  }
+
+  Widget userUid() {
+    return Text(user?.email ?? 'User email');
+  }
+
+  Widget signOutButton() {
+    return ElevatedButton(
+      onPressed: signOut,
+      child: const Text('Sign Out'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            userUid(),
+            signOutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
